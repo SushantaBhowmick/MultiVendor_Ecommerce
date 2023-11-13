5f6ba91c73e3346e09dbf5a765b5b848daa6a6eb
Sushanta Bhowmick <bhosushanta922@gmail.com>
2023-11-14 03:12:08 +0530

    add search filter

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,25 +1,24 @@
 import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'
 import './App.css';
-import {LoginPage,SignUpPage,ActivationPage} from './Routes.js'
-import {ToastContainer, toast} from 'react-toastify'
+import {LoginPage,SignUpPage,ActivationPage,HomePage} from './Routes.js'
+import {ToastContainer} from 'react-toastify'
 import "react-toastify/dist/ReactToastify.css";
 import { useEffect } from 'react';
-import { server } from './server.js';
-import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { loadUser } from './redux/actions/user.js';
 
 function App() {
-
+const dispatch=useDispatch();
 
   useEffect(()=>{
-    axios.get(`${server}/user/me`,{withCredentials:true})
-    .then(res=>toast.success(res.data.message))
-    .catch(err=>toast.error(err.response.data.message))
+    dispatch(loadUser())
   })
 
   return (
     <>
     <Router>
       <Routes>
+        <Route path='/' element={<HomePage/>} />
         <Route path='/login' element={<LoginPage/>} />
         <Route path='/sign-up' element={<SignUpPage/>} />
         <Route path='/activation/:activation_token' element={<ActivationPage/>} />
--- myapp/src/Routes.js
+++ myapp/src/Routes.js
@@ -1,9 +1,11 @@
 import LoginPage from "../src/pages/LoginPage.jsx";
 import SignUpPage from "../src/pages/SignUpPage.jsx";
 import ActivationPage from "../src/pages/ActivationPage.jsx";
+import HomePage from "../src/pages/HomePage.jsx";
 
 export{
     LoginPage,
     SignUpPage,
-    ActivationPage
+    ActivationPage,
+    HomePage
 }

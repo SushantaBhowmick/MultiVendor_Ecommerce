e5cca524c607f8c28bfbd026aa51b4dd53bed2ea
Sushanta Bhowmick <bhosushanta922@gmail.com>
2023-11-13 22:21:51 +0530

    add redux store

---
+++ myapp/src/redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { userReducers } from './reducers/user';
+
+const Store = configureStore({
+    reducer:{
+        user:userReducers,
+    }
+});
+
+
+export default Store;

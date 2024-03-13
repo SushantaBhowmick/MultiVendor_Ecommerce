2d048f9afac3dcb989e3f0aec3c47a42c9e9e3f9
Sushanta Bhowmick <bhosushanta922@gmail.com>
2024-03-13 09:36:03 +0530

    fixed the event page issue

--- myapp/src/components/Route/Events/EventCard.jsx
+++ myapp/src/components/Route/Events/EventCard.jsx
@@ -4,13 +4,12 @@ import CountDown from './CountDown.jsx'
 import { backend_url } from "../../../server.js";
 
 const EventCard = ({active,data}) => {
-  console.log(data)
   return (
     <>
       <div className={`w-full block bg-white rounded-lg ${active?"unset":"mb-12"} lg:flex p-2 mb-12`}>
         <div className="w-full lg:-w[50%] m-auto">
           <img
-            src={`${backend_url}${data.images[0]}`}
+            src={`${backend_url}${data && data.images[0]}`}
             alt="event"
           />
         </div>
@@ -22,15 +21,15 @@ const EventCard = ({active,data}) => {
           <div className="flex py-2 justify-between">
             <div className="flex">
                 <h5 className="font-[500] text-[18px] text-[#d55b45] pr-3 line-through">
-                    {data.originalPrice}$
+                    {data && data.originalPrice}$
                 </h5>
                 <h5 className="font-bold text-[20px] text-[#333] font-Roboto">
-                {data.discountPrice}$
+                {data && data.discountPrice}$
 
                 </h5>
             </div>
             <span className="pr-3 font-[400] text-[17px] text-[#44a55e]">
-                {data.stock} sold
+                {data && data.stock} sold
             </span>
           </div>
           <CountDown data={data} />
--- myapp/src/components/Route/Events/Events.jsx
+++ myapp/src/components/Route/Events/Events.jsx
@@ -6,20 +6,22 @@ import { useSelector } from "react-redux";
 const Events = () => {
   const { allEvents, isLoading } = useSelector((state) => state.event);
 
-  // useEffect(()=>{
-  //   const data = allEvents && allEvents.find((a,b)=>a.sold_out-b.sold_out)
-  //   console.log(data)
-  // },[allEvents])
-
   return (
     <div>
       {!isLoading && (
         <div className={`${styles.section}`}>
           <div className={`${styles.heading}`}>
-            <h1>Popular Eventes</h1>
+            <h1>Popular Events</h1>
           </div>
           <div className="w-full grid">
+            {
+              allEvents.length !==0 ? (
             <EventCard data={allEvents && allEvents[0]} />
+            // <h1>Hello</h1>
+              ):(
+                <h4>No Events Found!</h4>
+              )
+            }
           </div>
         </div>
       )}
